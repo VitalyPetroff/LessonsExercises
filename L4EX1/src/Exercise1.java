5f27f2dee7ce9272330f17750eb5906ac0922b18
petrov <petrovw@yandex.ru>
2017-11-22 22:24:19 +0300

    Hello!

--- L4EX1/src/Exercise1.java
+++ L4EX1/src/Exercise1.java
@@ -2,15 +2,59 @@ public class Exercise1 {
 
     public static void main(String[] args) {
         int a, b;
+        boolean t = true;
+        boolean f = false;
+
         a = (int)(Math.random()*10);
         b = (int)(Math.random()*10);
+        System.out.println("a = " + a + "; b = " + b);
         arithmetic(a, b);
+        binary(a, b);
+        System.out.println("t = " + t + " f = " + f);
+        logical(t, f);
+
+        condition(t);
+        ternary(t, a, b);
+        condition(f);
+        ternary(f, a, b);
+
     }
 
     private static void arithmetic(int a, int b){
+        System.out.println("АРИФМЕТИЧЕСКИЕ ОПЕРАЦИИ: ");
         System.out.println("a + b = " + (a + b));
         System.out.println("a - b = " + (a - b));
         System.out.println("a * b = " + (a * b));
         System.out.println("a * b = " + ((double)a / b));
+    }
+
+    private static void binary(int a, int b){
+        System.out.println("БИНАРНЫЕ ОПЕРАЦИИ: ");
+        System.out.println("a | b = " + (a | b));
+        System.out.println("a & b = " + (a & b));
+        System.out.println("a ^ b = " + (a ^ b));
+        System.out.println("a << 1 = " + (a << 1));
+        System.out.println("a >> 1 = " + (a >> 1));
+    }
+
+    private static void logical(boolean first, boolean second){
+        System.out.println("a || b = " + (first || second));
+        System.out.println("a && b = " + (first && second));
+        System.out.println("!a = " + !first);
+        System.out.println("!b = " + !second);
+    }
+
+    private static void condition(boolean condition){
+        if (condition){
+            System.out.println("TRUE!");
+        } else {
+            System.out.println("FALSE !");
+        }
+    }
+
+    private static void ternary(boolean condition, int first, int second){
+        int result;
+        result = (condition ? first : second);
+        System.out.println("Result = " + result);
     }
 }
