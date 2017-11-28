fec731e9591c47141c3f9d0caacc63ff1452d4cf
petrov <petrovw@yandex.ru>
2017-11-28 19:18:49 +0300

    Lesson7

---
+++ L7EX1/src/Lesson7.java
@@ -0,0 +1,15 @@
+public class Lesson7 {
+
+    public static void main(String[] args) {
+        print();
+    }
+
+    public static void print(){
+        System.out.println("Message");
+    }
+
+    public static void print(){
+        System.out.println("New Message");
+        System.out.println("New msg +1");
+    }
+}
