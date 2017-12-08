31a7168f3603987df6690ea35e27f1c38c481608
petrov <petrovw@yandex.ru>
2017-12-09 01:15:34 +0300

    CurrentSecond

--- L8EX2/src/CurrentSecondsTest.java
+++ L8EX2/src/CurrentSecondsTest.java
@@ -2,38 +2,35 @@ import java.util.Date;
 
 public class CurrentSecondsTest {
 
-    public static void main(String[] args) {
-        final int currentSecond1 = 49;
-        final int currentSecond2 = 57;
-        compareSeconds(currentSecond1);
-        compareSecondsRecursive(currentSecond2);
+    public static void main(String[] args) throws InterruptedException {
+        final int constSecond1 = 49;
+        final int constSecond2 = 53;
+        compareSeconds(constSecond1);
+        compareSecondsRecursive(constSecond2);
     }
 
-    public static void compareSeconds(int sec){
+    public static void compareSeconds(int constSec) throws InterruptedException {
+        int currentSecond;
         do {
-            if (sec == (new Date().getSeconds())) {
-                System.out.println("Текущая секунда равна константе " + sec);
-                break;
+            currentSecond = new Date().getSeconds();
+            if (currentSecond != constSec) {
+                System.out.println("Текущая секунда : " + currentSecond);
+                Thread.sleep(1000);
             } else {
-                try {
-                    Thread.sleep(1000);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
+                System.out.println("Текущая секунда равна заданной! " + currentSecond);
+                break;
             }
         } while (true);
     }
 
-    public static void compareSecondsRecursive(int sec){
-        if (sec == new Date().getSeconds()){
-            System.out.println("Текущая секунда равна константе " + sec);
+    public static void compareSecondsRecursive(int constSec) throws InterruptedException {
+        int currentSecond = new Date().getSeconds();
+        if (currentSecond != constSec) {
+            System.out.println("Текущая секунда : " + currentSecond);
+            Thread.sleep(1000);
+            compareSecondsRecursive(constSec);
         } else {
-            try{
-                Thread.sleep(1000);
-                compareSeconds(sec);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
+            System.out.println("Текущая секунда равна заданной! " + currentSecond);
         }
     }
 }
