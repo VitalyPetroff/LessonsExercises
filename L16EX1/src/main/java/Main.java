77d3b041d610175be2eb62f8330e1c1d7b945c91
petrov <petrovw@yandex.ru>
2018-01-02 23:22:18 +0300

    MVC-Model

--- L16EX1/src/main/java/Main.java
+++ L16EX1/src/main/java/Main.java
@@ -3,7 +3,7 @@ import static spark.Spark.post;
 
 public class Main {
     public static void main(String[] args) {
-        get("/simpleGet", (request, responce) -> "HiThereItsGetRequest");
+        get("/simpleGet", (request, responce) -> "Request");
         post("/simplePost", (request, responce) -> "PostRequestHaHa");
     }
 }
